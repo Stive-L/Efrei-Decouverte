07e087f209dcad29e8bc7db21ce3e6a68202407c
Stive <zhuo-chan-stive.lee@efrei.net>
2025-04-28 20:21:54 +0200

    patch

--- frontend/js/authFetch.js
+++ frontend/js/authFetch.js
@@ -1,13 +1,20 @@
 // js/authFetch.js
-export function authFetch(url, options = {}) {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    alert('Token manquant');
-    throw new Error('Token manquant');
-  }
-  options.headers = {
-    ...(options.headers || {}),
-    'Authorization': `Bearer ${token}`
-  };
-  return fetch(url, options);
+window.authFetch = function(url, options = {}) {
+    // Copie les options pour ne pas les modifier globalement
+    const opts = Object.assign({}, options);
+
+    // Ajoute les headers s'ils n'existent pas déjà
+    opts.headers = opts.headers || {};
+
+    // Ajoute le token s'il existe
+    const token = localStorage.getItem('token');
+    if (token) {
+        opts.headers['Authorization'] = 'Bearer ' + token;
+    }
+    // Pour du JSON
+    if (!opts.headers['Content-Type']) {
+        opts.headers['Content-Type'] = 'application/json';
+    }
+
+    return fetch(url, opts);
 }
